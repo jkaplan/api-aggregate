056d1439268bea8eae7079da7f64b53621e92093
Jason <jason.kaplan88@gmail.com>
2022-06-07 12:32:22 -0500

    Added laravel breeze ui

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Api\StreamerController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
